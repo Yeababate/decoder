d8f344c2e7ea105e76f9fc463c2c2b40724b1d05
Yeabsira Abate Guangul <k-yguangu@c1r1p5.hive.fi>
2026-05-03 12:28:51 +0300

    optimized a bit

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 "os"
 )
 
-
 func WriteRep(length int, rep string) string{
 	Changed := ""
 	for i := 0; i < length; i++ {
@@ -18,6 +17,8 @@ func WriteRep(length int, rep string) string{
 
 func Check(ToBeChecked byte) bool {
 	if !((ToBeChecked >= '0') && (ToBeChecked <= '9')) && (ToBeChecked != ' ') {
+		fmt.Println("input malformed\n")
+		os.Exit(0)
 		return true
 	}else {
 		return false
@@ -38,9 +39,7 @@ func BracketCheck(input string) bool{
 }
 
 func decoder(input string) string {
-	store := ""
-	output := ""
-	rep := ""
+	var store, output, rep string
 	var i,j,k int
 	for i = 0; i < len(input); i++{
 		if input[i] == '[' {
@@ -50,16 +49,8 @@ func decoder(input string) string {
 				}
 			for j = i+1; j < len(input); j++ {
 				if (input[j] >= '0' && input[j] <= '9') {
-					if Check((input[j+1])) == true {
-						fmt.Println("Arguments should be separated by comma\n")
-						os.Exit(0)
-					}
-					// if !((input[j+1] >= '0') && (input[j+1] <= '9')) && (input[j+1] != ' ') {
-					// 	// fmt.Println(string(input[j]))
-					// 	// fmt.Println(string(input[j+1]))
-					// 	fmt.Println("Arguments should be separated by comma")
-					// 	os.Exit(0)
-					// }
+					Check(input[j+1])
+
 					store += string(input[j])
 				} else if input[j] == ' ' {
 					length, err := strconv.Atoi(store)
@@ -83,14 +74,15 @@ func decoder(input string) string {
 					break
 				}
 			}
-		}else {
+		} else {
 			output += string(input[i])
 		}
 	}
 	return output
 }
+
 func main() {
-	input := "[5 a][5 -_]-[5 #]"
+	input := "[14 a][8 -_]***[10 #]"
 	BracketCheck(input)
 	fmt.Println(decoder(input))
 }
